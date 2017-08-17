f2fc4f5c3e430f01decc6e9b6f8a9baa0baf97d4
Ali Hawke <quantumtea@gmail.com>
2017-08-17 18:38:40 -0500

    Didn't need a dependency

--- mix.exs
+++ mix.exs
@@ -29,7 +29,6 @@ defmodule HelloElixir.Mixfile do
   defp deps do
     [{:httpotion, "~> 3.0.2"}, 
     {:poison, "~> 3.0"}, 
-    {:timex, "~> 3.0"},
-    {:basic_auth, "~> 2.1.4"}]
+    {:timex, "~> 3.0"}]
   end
 end
