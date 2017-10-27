8797e33bb33aab558d905b623ab275b89a17de0f
Alison Hawke <quantumtea@gmail.com>
2017-10-27 08:34:59 -0500

    Starting region history

--- lib/nanowrimo.ex
+++ lib/nanowrimo.ex
@@ -4,17 +4,40 @@ defmodule NaNoWriMo do
 	def getRegionalXML(regionstring) do
 		url = "https://nanowrimo.org/wordcount_api/wcregion/" <> regionstring
 		response = HTTPotion.request(:get, url)
-		parseXML(response.body)
+		parseOutUsefulData(response.body)
 	end
 
 	def getRegionalXMLFromFile() do
 		{:ok, contents} = File.read("./res/nanowrimo_region.xml")
-		parseXML(contents)
+		parseDonationsFromFile(contents)
 	end	
 
-	def parseXML(xml) do
-		result = xml |> xpath(~x"//wcregion/donations/text()") # `sigil_x` for (x)path
-		# IO.puts("\n donations: #{result}")
-		result
+	def parseDonationsFromFile(xml) do
+		xml |> xpath(~x"//wcregion/donations/text()") # `sigil_x` for (x)path
 	end
+
+	def parseOutUsefulData(xml) do
+		donations = xml |> xpath(~x"//wcregion/donations/text()") # `sigil_x` for (x)path
+		donors = xml |> xpath(~x"//wcregion/numdonors/text()") # `sigil_x` for (x)path
+		totalWordsWritten = xml |> xpath(~x"//wcregion/region_wordcount/text()") # `sigil_x` for (x)path
+		totalPeople = xml |> xpath(~x"//wcregion/numparticipants/text()") # `sigil_x` for (x)path
+		
+		IO.puts("\n donations: #{donations}")
+		IO.puts(" halos: #{donors}")
+		IO.puts(" word count: #{totalWordsWritten}")
+		IO.puts(" people: #{totalPeople}")
+
+		donations
+	end
+
+	def getRegionalWordcountHistoryFromFile() do
+		{:ok, contents} = File.read("./res/nanowrimo_region_history.xml")
+		parseRegionHistoryFromFile(contents)
+	end
+
+	def parseRegionHistoryFromFile(xml) do
+		# get all the wcentry/wc elements
+		# deliver the totals, you can dump it in the spreadsheet later
+	end
+
 end
