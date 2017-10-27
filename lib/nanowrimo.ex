6c5c5d1f834d1094644d32927f5bed2b0562a940
Alison Hawke <quantumtea@gmail.com>
2017-10-27 08:10:23 -0500

    Working from endpoint!

--- lib/nanowrimo.ex
+++ lib/nanowrimo.ex
@@ -3,26 +3,18 @@ defmodule NaNoWriMo do
 
 	def getRegionalXML(regionstring) do
 		url = "https://nanowrimo.org/wordcount_api/wcregion/" <> regionstring
-		{:ok, response} = HTTPotion.request(:get, url)
-		parseXMLFromEndpoint(response.body)
+		response = HTTPotion.request(:get, url)
+		parseXML(response.body)
 	end
 
 	def getRegionalXMLFromFile() do
 		{:ok, contents} = File.read("./res/nanowrimo_region.xml")
-		parseXMLFromFile(contents)
+		parseXML(contents)
 	end	
 
-	def parseXMLFromFile(xml) do
+	def parseXML(xml) do
 		result = xml |> xpath(~x"//wcregion/donations/text()") # `sigil_x` for (x)path
-		IO.puts("\n donations from file: #{result}")
+		# IO.puts("\n donations: #{result}")
 		result
-	end
-
-	def parseXMLFromEndpoint(xml) do
-		IO.puts("\n response body\n #{xml}")
-		# works for the file, not for the live endpoint
-		result = xml |> xpath(~x"//wcregion/donations/text()") # `sigil_x` for (x)path
-		IO.puts("\n donations from endpoint: #{result}")
-		#result
 	end
 end
