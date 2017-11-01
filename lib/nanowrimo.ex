246636e7d7c122ca97daf336b1425ef3b3b6bc16
Alison Hawke <quantumtea@gmail.com>
2017-11-01 09:14:14 -0500

    Getting live data from NaNoWriMo

--- lib/nanowrimo.ex
+++ lib/nanowrimo.ex
@@ -4,7 +4,7 @@ defmodule NaNoWriMo do
 	def getRegionalXML(regionstring) do
 		url = "https://nanowrimo.org/wordcount_api/wcregion/" <> regionstring
 		response = HTTPotion.request(:get, url)
-		parseOutUsefulData(response.body, regionstring)
+		parseUsefulData(response.body, regionstring)
 	end
 
 	def getRegionalXMLFromFile() do
@@ -16,7 +16,7 @@ defmodule NaNoWriMo do
 		xml |> xpath(~x"//wcregion/donations/text()") # `sigil_x` for (x)path
 	end
 
-	def parseOutUsefulData(xml, regionstring) do
+	def parseUsefulData(xml, regionstring) do
 		donations = xml |> xpath(~x"//wcregion/donations/text()") 
 		donors = xml |> xpath(~x"//wcregion/numdonors/text()") 
 		totalWordsWritten = xml |> xpath(~x"//wcregion/region_wordcount/text()") 
@@ -33,20 +33,27 @@ defmodule NaNoWriMo do
 
 	def getRegionalWordcountHistoryFromFile() do
 		{:ok, contents} = File.read("./res/nanowrimo_region_history.xml")
-		parseRegionHistory(contents)
+		parseRegionHistoryFromFile(contents)
 	end
 
 	def getRegionalWordcountHistory(regionstring) do
-		url = "http://nanowrimo.org/wordcount_api/wcregionhist/" <> regionstring
+		url = "https://nanowrimo.org/wordcount_api/wcregionhist/" <> regionstring
 		response = HTTPotion.request(:get, url)
+		# IO.puts response
 		parseRegionHistory(response.body)
 	end
 
-	def parseRegionHistory(xml) do
+	def parseRegionHistoryFromFile(xml) do
 		# get all the wc elements
 		history = xml |> xpath(~x"//wordcounts/wcentry/wc/text()"l) # `l` stands for (l)ist
-		# new line to make it easy to paste these into a spreadsheet
 		IO.puts("\n")
+		Enum.each(history, fn(x) -> IO.puts(x) end)
+		history
+	end
+
+	def parseRegionHistory(xml) do
+		history = xml |> xpath(~x"//wcregion/wordcounts/wcentry/max/text()"l) # `l` stands for (l)ist
+		IO.puts("\nRegion history")
 		Enum.each(history, fn(x) -> IO.puts(x) end)
 		history
 	end
--- test/nanowrimo_test.exs
+++ test/nanowrimo_test.exs
@@ -2,34 +2,35 @@ defmodule NaNoWriMoTest do
   use ExUnit.Case
   doctest NaNoWriMo
 
-  test "Get the XML from the file" do
-    donations = NaNoWriMo.getRegionalXMLFromFile()
-    assert(donations == '1315.0')
-  end
+  # test "Get the XML from the file" do
+    # donations = NaNoWriMo.getRegionalXMLFromFile()
+    # assert(donations == '1315.0')
+  # end
 
   test "Get the donation amount for region from NaNoWriMo" do
     donations = NaNoWriMo.getRegionalXML("usa-missouri-st-louis")
     assert(donations >= '1335.0')
   end
 
-  test "Get the regional word count history from file" do
-    history = NaNoWriMo.getRegionalWordcountHistoryFromFile()
-    assert(Enum.count(history) == 5) 
+  # test "Get the regional word count history from file" do
+    # history = NaNoWriMo.getRegionalWordcountHistoryFromFile()
+    # assert(Enum.count(history) == 5) 
     # alternative to the above statement is
     # assert(Kernel.length(history) == 5)
-  end
+  # end
 
   test "Get the regional word count history from NaNoWriMo" do
-    # history = NaNoWriMo.getRegionalWordcountHistory("usa-missouri-st-louis")
+    history = NaNoWriMo.getRegionalWordcountHistory("usa-missouri-st-louis")
     # dayNumber = Timex.Parse.DateTime.Parser.parse(Timex.today, "{D}")
+    # IO.puts ("Day number: #{dayNumber}")
     # should be an entry for every day so far in this month
     # this will only work for November
-    # assert(Enum.count(history) == dayNumber) 
+    assert(Enum.count(history) == 1) 
   end
   
-  test "Get regional rank from file" do
+  # test "Get regional rank from file" do
    # rank = NaNoWriMo.getRegionalRank("USA :: Missouri :: St. Louis")
    # assert String.contains?(rank, "USA :: Missouri :: St. Louis")
-  end
+  # end
 
 end
