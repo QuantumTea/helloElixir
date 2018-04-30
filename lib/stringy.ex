9ec8cf0d26638cfdfd7cf52d43e7b5de65e2eaaf
Alison Hawke <quantumtea@gmail.com>
2018-04-30 16:13:33 -0500

    Counting the number of letter e - ASH

--- lib/stringy.ex
+++ lib/stringy.ex
@@ -5,16 +5,35 @@ defmodule Stringy do
 		valx <> valy
 	end
 
-	def bottlesOfBeerOnTheWall(bottles) do
-		case bottles do
-			1 -> "1 bottle of beer on the wall."
-			0 -> "No bottles of beer on the wall, go to the store and get some more."
-			23 -> "23 bottles of beer on the wall."
-			_ -> "some other number of bottles here."
-		end
+	def bottlesOfBeerOnTheWall(bottles) when bottles == 1 do
+		"1 bottle of beer on the wall."
 	end
+
+	def bottlesOfBeerOnTheWall(bottles) when bottles > 1 do
+		numberOfBottles = Integer.to_string(bottles)
+		numberOfBottles <> " bottles of beer on the wall."
+	end
+
+	def bottlesOfBeerOnTheWall(bottles) when bottles == 0 do
+		"No bottles of beer on the wall, go to the store and get some more."
+	end
+
+	def bottlesOfBeerOnTheWall(bottles) when bottles < 0 do
+		"How can you have a negative number of bottles?"
+	end
+
+	# Using guards was tidier and easier than using case
+	# def bottlesOfBeerOnTheWall(bottles) do
+	# 	case bottles do
+	# 		0 -> "No bottles of beer on the wall, go to the store and get some more."
+	# 		1 -> "1 bottle of beer on the wall."
+	# 		2 -> "2 bottles of beer on the wall."
+	# 		_ -> "plenty of bottles here."
+	# 	end
+	# end
 	
 	def countTheEs(phrase) do
-		3
+		result = String.split(phrase, "e")
+		Enum.count(result) - 1
 	end
 end
--- test/stringy_test.exs
+++ test/stringy_test.exs
@@ -9,12 +9,15 @@ defmodule StringysTest do
   test "can count bottles of beer on the wall" do
     assert Stringy.bottlesOfBeerOnTheWall(1) == "1 bottle of beer on the wall."
     assert Stringy.bottlesOfBeerOnTheWall(0) == "No bottles of beer on the wall, go to the store and get some more."
-    assert Stringy.bottlesOfBeerOnTheWall(23) == "23 bottles of beer on the wall."
-    assert Stringy.bottlesOfBeerOnTheWall(47) == "some other number of bottles here."
+    assert Stringy.bottlesOfBeerOnTheWall(47) == "47 bottles of beer on the wall."
+    assert Stringy.bottlesOfBeerOnTheWall(-3) == "How can you have a negative number of bottles?"
   end
 
   test "can count the number of e's in a given phrase" do
     assert Stringy.countTheEs("The quick brown fox jumps over the lazy dog") == 3
+    assert Stringy.countTheEs("Hello world!") == 1
+    assert Stringy.countTheEs("eeeeeeeeee") == 10
+    assert Stringy.countTheEs("eieio eieio") == 4
   end
-  
+
 end
